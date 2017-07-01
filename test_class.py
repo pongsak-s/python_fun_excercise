9f1bbd2570c1d5c4f7e5129d3e26da7bddbcd19c
Pongsak Srithongnopawong <pongsaksrithongnopawong@Pongsaks-MacBook-Pro.local>
2017-07-01 17:44:29 +0700

    test class

---
+++ test_class.py
@@ -0,0 +1,15 @@
+
+
+class Test():
+	"""test class"""
+	def __init__(self):
+		self.stacks = []
+		print("init the stack")
+
+	def testRun(self):
+		print("test run")
+
+
+test = Test()
+test.testRun()
+		
