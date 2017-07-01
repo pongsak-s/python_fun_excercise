dfd78ffcaefbede6fc978615b2849cc7b2b5971b
Pongsak Srithongnopawong <pongsaksrithongnopawong@Pongsaks-MacBook-Pro.local>
2017-07-01 19:17:49 +0700

    enumerate sample

---
+++ enumerate.py
@@ -0,0 +1,7 @@
+
+
+x = range(10)
+
+for i, x_i in enumerate(x):
+	print(x_i)
+
--- listcomprehen.py
+++ listcomprehen.py
@@ -1,2 +1,9 @@
 
 
+x = range(100)
+
+
+print([(i,x_i) #create tuple (immutable list) 
+	for i,x_i in enumerate(x) # extract index and value
+	if (i is not 0) and (x_i % i == 0)  ]) # condition to craft list
+
